622c4d0fe7763614907402f6c0061b3363639509
Muna Gigowski <munagigowski@gmail.com>
2019-03-17 09:31:52 -0400

    complete commit

--- CIS371HW7/myJSFile.js
+++ CIS371HW7/myJSFile.js
@@ -1,105 +1,215 @@
 // JavaScript Document
 
+var db = firebase.database();
+var rootRef = db.ref();
+var stock = new Object();
+var firstTime = true;
+//friendRef = rootRef.child(“friend”);
+
+rootRef.on('child_added', addTable);
+rootRef.on('child_removed', removeTableRow);
+//rootRef.on('value', addFriend);
+
 function processUpload() {
   var uploadInfo = document.getElementById('upload');
   if ('files' in uploadInfo) {
     var reader = new FileReader();
     reader.onload = function(event) {
-      var jsonObj = JSON.parse(event.target.result);
+      var jsonObj = JSON.parse(event.target.result); //an array of objects
       /* put your code here, whatever you need to do with jsonObj */
-	  console.log(jsonObj);
-	  addTable(jsonObj);
+	  for(var i = 0; i < jsonObj.length; i++) {
+	       rootRef.push().set(jsonObj[i]);  
+	  }
     };
     /* file is an array and we are interested only in the first element */
     reader.readAsText(uploadInfo.files[0]);
   }
 }
 
-function addTable(jsonObj) {
+function addTable(snapshot) {
+	
 
-            var myTableDiv = document.getElementById("metric_results")
-            var table = document.createElement('TABLE')
-            var tableBody = document.createElement('TBODY')
-
-            table.border = '1'
-            table.appendChild(tableBody);
-
-            var heading = new Array();
-            heading[0] = "Name"
-            heading[1] = "Phone Number"
-            heading[2] = "Age"
-			heading[3] = "Action"
-
-            var stock = new Array();
+            var myTable = document.getElementById("friendTable");
+			var div = myTable.parentNode;
+			var temp;
 			
-			//creates an array of arrays that are a name->value pairs
-			for(var x in jsonObj) {
-				//if var is a phone number, then take value and format it
-		    	if (jsonObj.hasOwnProperty(x)) {
-        			stock.push(x, jsonObj[x]);
-   				 }
+			stock[snapshot.key] = snapshot.val();
+			
+			for(var key in stock) {
+				if(stock.hasOwnProperty(key)) {
+					var x = stock[key].phone;	
+					if(x.length == 7 || x.length == 10) {
+						if(x.length == 7) {
+							temp = x.substring(0,3) + "-" + x.substring(3);
+						}
+						if(x.length == 10) {
+							temp = "(" + x.substring(0,3) + ")" + x.substring(3,6) + "-" + x.substring(6);
+						}
+						stock[key].phone = temp;
+					}
+				}
+				
 			}
 
-			
-			//stock[0] = new Array("Cars", "88.625", "85.50", "85.81", "987")
-            //stock[1] = new Array("Veggies", "88.625", "85.50", "85.81", "988")
+
+			//var stockSet = new Set(stock);
+			//stock = Array.from(stockSet);
+
 
             //TABLE COLUMNS
-            for (i = 0; i < stock.length; i++) {
 				var tr = document.createElement('TR');
-				for (j = 0; j < stock[i].length; j++) {
-					var td = document.createElement('TD')
-					td.appendChild(document.createTextNode(stock[i][j]));
-					tr.appendChild(td)
-				}
-				tableBody.appendChild(tr);
-			}
-
-            //TABLE ROWS
-            var tr = document.createElement('TR');
-            tableBody.appendChild(tr);
-
-            for (i = 0; i < stock.length; i++) {
-                for (j = 0; j < stock[i].length; j++) {
-                    var td = document.createElement('TD')
-					if(i == ) {
-                    	td.appendChild(document.createTextNode(stock[i][j]));
-					} else {
-						//create button for deleting rows
+				for (var val in stock[snapshot.key]) {
+					if(stock[snapshot.key].hasOwnProperty(val)) {
+						var td = document.createElement('TD')
+						td.appendChild(document.createTextNode(stock[snapshot.key][val]));
+						tr.appendChild(td)
 					}
-                    td.appendChild(td)
-                }
-            }
+				}
+				var td = document.createElement('TD')
+				var colButton = document.createElement('BUTTON');
+				colButton.appendChild(document.createTextNode("Delete"));
+				colButton.setAttribute('onclick', `removeRow("${snapshot.key}")`);
+				td.appendChild(colButton);
+				tr.appendChild(td)
+				myTable.appendChild(tr);
 
-            myTableDiv.appendChild(table)
+
+			if(firstTime) {
+				if(document.getElementById("deleteAll") == null) {
+					var deleteAll = document.createElement('button');
+					deleteAll.id = "deleteAll";
+					deleteAll.innerHTML = "Delete All Records";
+					deleteAll.setAttribute('onclick', "removeAllRows()");
+					div.appendChild(deleteAll);
+				}
+			div.className = "center";
 			
-			var deleteAll = document.createElement('button');
-			deleteAll.id = "deleteAll";
-			var body = document.getElementByTagName("BODY");
-			body.appendChild(deleteAll);
-			addInsertData();
+			firstTime = false;
+			
+			}
 
         }
 		
-		function addInsertData() {
-			
-			var body = document.getElementByTagName("BODY");
-			var insertForm = document.createElement("FORM");
-			var 
-			
-		}
-		
 		function validateInputData() {
-		
-		//first letter up, others lower: /^[A-Z][a-z]$/	
-		//to read already-formatted 10 digit number: ^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$
-		//Check phone number: (^[0-9]{7} | ^[0-9]{10})$
-		//Age: ^[0-9]{1,2}$
 			
+			var nameValue = document.getElementById("nameField").value;
+			var numberValue = document.getElementById("numberField").value;
+			var ageValue = document.getElementById("ageField").value;
+			
+			var allPassed = true;
+			
+			if(/[A-Z]{1}[a-z]+/.test(nameValue)) {
+				
+			} else {
+				allPassed = false;
+				alert("Name formatting incorrect. Please re-enter.");
+			}
+			if(/^([0-9]{7})$|^([0-9]{10})$/.test(numberValue)) {
+				
+			} else {
+				allPassed = false;
+				alert("Invalid phone number. Please re-enter.");
+			}
+			if(/^[0-9]{1,2}$/.test(ageValue)) {
+				
+			} else {
+				allPassed = false;
+				alert("Inavlid age. Please re-enter.");
+			}
+			
+			if(allPassed) {
+				addFriend(nameValue, numberValue, ageValue);
+			}
 		}
 		
 		function searchDatabase() {
-			//first letter up, others lower: /^[a-z][A-Z]{1, }$/	
 			
+			var searchValue = document.getElementById("searchField").value;
+			
+			if(/[A-Za-z]+/.test(searchValue)) {
+				
+			var resultsTable = document.getElementById("resultsTable");
+			var parentDiv = resultsTable.parentNode;
+			parentDiv.className = "show";
+			
+			//Clear table of all possible previous values
+			var oldContent = document.querySelectorAll("#resultsTable tr");
+			var oldLength = resultsTable.rows.length;
+			
+			for(var i = 1; i < oldLength; ++i) {
+				resultsTable.deleteRow(1);
+			}
+			}
+			rootRef.orderByChild('name').on('child_added', executeQuery);
+		}
+		
+		function executeQuery(snapshot) {
+			
+			var searchValue = document.getElementById("searchField").value;
+			var resultsTable = document.getElementById("resultsTable");
+			
+			if(/[A-Za-z]+/.test(searchValue)) {
+			var results = new Object();
+			results[snapshot.key] = snapshot.val();
+			
+			var valid = new Object();
+			
+			for(var val in results) {
+				if(results.hasOwnProperty(val)) {
+					if(results[snapshot.key].name.toLowerCase().includes(searchValue.toLowerCase())) {
+						valid[snapshot.key] = snapshot.val();
+					}
+				}
+				
+			}
+			
+			    //TABLE COLUMNS
+				var tr = document.createElement('TR');
+				for (var val in valid[snapshot.key]) {
+					if(valid[snapshot.key].hasOwnProperty(val)) {
+						var td = document.createElement('TD')
+						td.appendChild(document.createTextNode(valid[snapshot.key][val]));
+						tr.appendChild(td)
+						resultsTable.appendChild(tr);
+					}
+				}
+				
+
+			 }
+		}
+		
+		function removeRow(key) {
+			
+			var childRef = rootRef.child(key);
+			childRef.remove();
+		}
+		
+		function removeTableRow(snapshot) {
+			
+			var name = snapshot.val().name;
+			
+			var rows = document.getElementsByTagName("tr");
+			for (var i = rows.length; i--;) {
+			  if(rows[i].innerHTML.indexOf(name) !== -1) {
+				rows[i].parentNode.removeChild( rows[i] );
+			  }
+			}
+			
+		}
+		
+		function removeAllRows() {
+			rootRef.remove();
+			
+			var emptyTable = document.getElementById("friendTable");
+			var d = emptyTable.parentNode;
+			
+			d.className = "hidden";
+			firstTime = true;
+		}
+		
+		function addFriend(nameValue, numberValue, ageValue) {
+			
+			rootRef.push().set({ name: nameValue, phone: numberValue, age: ageValue });
+
 		}
 		
