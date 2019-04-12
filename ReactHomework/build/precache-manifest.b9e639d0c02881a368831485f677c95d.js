f05b8a0b4d6c59238eaceeb6dd6c320b336b5be4
Muna Gigowski <munagigowski@gmail.com>
2019-04-12 12:28:04 -0400

    first commit

---
+++ ReactHomework/build/precache-manifest.b9e639d0c02881a368831485f677c95d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Muna72/Muna72.github.io/tree/master/ReactHomework/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2bdafd2a51045f000c07",
+    "url": "/Muna72/Muna72.github.io/tree/master/ReactHomework/static/js/runtime~main.e251c136.js"
+  },
+  {
+    "revision": "6d147a75e2edc614245f",
+    "url": "/Muna72/Muna72.github.io/tree/master/ReactHomework/static/js/main.b9a63911.chunk.js"
+  },
+  {
+    "revision": "618c1ecfe1afcbcee49c",
+    "url": "/Muna72/Muna72.github.io/tree/master/ReactHomework/static/js/2.9573ae56.chunk.js"
+  },
+  {
+    "revision": "6d147a75e2edc614245f",
+    "url": "/Muna72/Muna72.github.io/tree/master/ReactHomework/static/css/main.fc9e5432.chunk.css"
+  },
+  {
+    "revision": "ea40fd84e39defc7092fd9c88e6951d5",
+    "url": "/Muna72/Muna72.github.io/tree/master/ReactHomework/index.html"
+  }
+];
