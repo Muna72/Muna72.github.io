f1182afb25c77b6365f78feae961f2f8fc894e65
Muna Gigowski <munagigowski@gmail.com>
2019-04-12 12:38:50 -0400

    second try

---
+++ ReactHomework/build/precache-manifest.b560c7611a498f7b86e59a3bb2c41508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactHomework/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7eceacf72d0f491f54c9",
+    "url": "/ReactHomework/static/js/runtime~main.4660e733.js"
+  },
+  {
+    "revision": "bcb11d3807f9db453705",
+    "url": "/ReactHomework/static/js/main.e02cc244.chunk.js"
+  },
+  {
+    "revision": "618c1ecfe1afcbcee49c",
+    "url": "/ReactHomework/static/js/2.9573ae56.chunk.js"
+  },
+  {
+    "revision": "bcb11d3807f9db453705",
+    "url": "/ReactHomework/static/css/main.fc9e5432.chunk.css"
+  },
+  {
+    "revision": "3652759d7f03ec8686f7a734fbefc628",
+    "url": "/ReactHomework/index.html"
+  }
+];
--- ReactHomework/build/service-worker.js
+++ ReactHomework/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Muna72/Muna72.github.io/tree/master/ReactHomework/precache-manifest.b9e639d0c02881a368831485f677c95d.js"
+  "/ReactHomework/precache-manifest.b560c7611a498f7b86e59a3bb2c41508.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Muna72/Muna72.github.io/tree/master/ReactHomework/index.html", {
+workbox.routing.registerNavigationRoute("/ReactHomework/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
